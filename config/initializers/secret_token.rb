a3df9acad289fbf4daba40ea3688a8b4247f9715
Danilo Caetano <dcaetano@itriagehealth.com>
2016-04-16 21:41:20 -0600

    Init the project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ArsenalApi::Application.config.secret_key_base = '689db7170b9c5b1f359b97145d2b0c6a95415f8a9bee9898c4b9c684f9102310cbe420d4b3a063a2330a1d4bd8b398996b555606bf215567a06b1fba6c77517c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ArsenalApi::Application.config.session_store :cookie_store, key: '_arsenal-api_session'
