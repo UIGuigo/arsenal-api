c6d0769d2c1864a3d24d846901b655f895ca32f7
Danilo Caetano <dcaetano@itriagehealth.com>
2016-04-16 23:49:04 -0600

    Updates application controller to prevent CSRF exception from being raised

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ ArsenalApi::Application.routes.draw do
     scope module: :v1,
               constraints: ApiConstraints.new(version: 1, default: true) do
       # List 'v1' resources here
+      resources :users, :only => [:show]
     end
   end
 end
