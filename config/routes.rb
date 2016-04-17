52fa9c304799369321c7c0583f5c84282d77bc30
Danilo Caetano <dcaetano@itriagehealth.com>
2016-04-16 22:42:25 -0600

    Set route constaints for API

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,9 @@
 ArsenalApi::Application.routes.draw do
+  # API definition
+  namespace :api, 
+            defaults: { format: :json },
+            constraint: { subdomains: 'api' }, 
+            path: '/' do
+    # List resources here
+  end
 end
